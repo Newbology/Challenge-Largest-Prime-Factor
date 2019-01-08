59afaa611b6cafa05bbd0e72387b0ed58690dfd9
kevin tauzer <kevint@kevins-MBP.home>
2019-01-08 10:08:13 -1000

    passed tests for challenge

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,19 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+exports.largestPrimeFactor = function(num) {
+  let primeFactors = [];
+  while (num % 2 === 0) {
+    primeFactors.push(2);
+    num = num / 2;
+  }
+  let sqrtNum = Math.sqrt(num);
+  for (let i = 3; i <= sqrtNum; i++) {
+    while (num % i === 0) {
+      primeFactors.push(i);
+      num = num / i;
+    }
+  }
+  if (num > 2) {
+    primeFactors.push(num);
+  }
 
-  return primeNumber;
+  return primeFactors.pop();
 };
